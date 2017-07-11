c258b2fceaeec61c424d995069a724b7fe0b4343
vrk <vrkwork@gmail.com>
2017-07-11 10:58:33 -0400

    create character class

--- public/js/character.js
+++ public/js/character.js
@@ -1,3 +1,37 @@
+// Base class
 class Character {
-  
+  update(direction) {
+    switch (direction) {
+      case MOVE_DOWN:
+        this.sprite = this.downSprite;
+      break;
+      case MOVE_UP:
+        this.sprite = this.upSprite;
+      break;
+      case MOVE_LEFT:
+        this.sprite = this.leftSprite;
+      break;
+      case MOVE_RIGHT:
+        this.sprite = this.rightSprite;
+      break;
+    }
+    this.sprite.update();
+  }
+
+  render(x, y) {
+    this.sprite.render(x, y);
+  }
+}
+
+class Bijou extends Character {
+  constructor(context) {
+    super();
+    const bijouImage = new Image();
+    bijouImage.src = 'images/bijou-sheet.png';
+
+    this.downSprite = new Sprite(context, 37, 34, 2, 64, 4, bijouImage);
+    this.upSprite = new Sprite(context, 37, 34, 2, 64+74, 4, bijouImage);
+    this.leftSprite = new Sprite(context, 32, 34, 2+296, 64+37, 4, bijouImage);
+    this.rightSprite = new Sprite(context, 32, 34, 2, 64+37, 4, bijouImage);
+  }
 }
--- public/js/player.js
+++ public/js/player.js
@@ -14,41 +14,18 @@ class Player {
     this.y = 0;
     this.arrowsPressed = [];
 
-    const bijouImage = new Image();
-    bijouImage.src = 'images/bijou-sheet.png';
-
-    this.downSprite = new Sprite(context, 37, 34, 2, 64, 4, bijouImage);
-    this.upSprite = new Sprite(context, 37, 34, 2, 64+74, 4, bijouImage);
-    this.leftSprite = new Sprite(context, 32, 34, 2+296, 64+37, 4, bijouImage);
-    this.rightSprite = new Sprite(context, 32, 34, 2, 64+37, 4, bijouImage);
-
-    this.sprite = this.downSprite;
+    this.character = new Bijou(context);
     this.direction = MOVE_DOWN;
   }
 
   update() {
     this.x += this.xVelocity;
     this.y += this.yVelocity;
-
-    switch (this.direction) {
-      case MOVE_DOWN:
-        this.sprite = this.downSprite;
-      break;
-      case MOVE_UP:
-        this.sprite = this.upSprite;
-      break;
-      case MOVE_LEFT:
-        this.sprite = this.leftSprite;
-      break;
-      case MOVE_RIGHT:
-        this.sprite = this.rightSprite;
-      break;
-    }
-    this.sprite.update();
+    this.character.update(this.direction);
   }
 
   render() {
-    this.sprite.render(this.x, this.y);
+    this.character.render(this.x, this.y);
   }
 
   // Private
