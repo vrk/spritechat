032d04383f4337c8a30fdd88e3512622b6d2d53d
vrk <vrkwork@gmail.com>
2017-07-11 13:33:59 -0400

    add grass

--- public/js/game.js
+++ public/js/game.js
@@ -3,16 +3,21 @@ class Game {
     this.startGameLoop = this.startGameLoop.bind(this);
     this._onReceiveServerMessage = this._onReceiveServerMessage.bind(this);
 
-    this.canvas = document.querySelector('canvas');
+    this.canvas = document.querySelector('#foreground');
     this.canvas.width = CANVAS_WIDTH;
     this.canvas.height = CANVAS_HEIGHT;
     this.context = this.canvas.getContext('2d');
     this.context.imageSmoothingEnabled = false;
 
+    this.backCanvas = document.querySelector('#background');
+    this.backCanvas.width = CANVAS_WIDTH;
+    this.backCanvas.height = CANVAS_HEIGHT;
+    this.backContext = this.backCanvas.getContext('2d');
+
     this._socket = socket;
     this.player = new Player(this.context, socket);
     this.others = {};
-    this.world = new World(this.context);
+    this.world = new World(this.backContext);
 
     this._socket.addEventListener('message', this._onReceiveServerMessage);
   }
@@ -39,11 +44,11 @@ class Game {
   }
 
   startGameLoop() {
+    this.world.render();
     const gameLoop = () => {
     this.context.clearRect(0, 0, CANVAS_WIDTH, CANVAS_HEIGHT);
       requestAnimationFrame(gameLoop);
       // TODO: Move this to a background layer.
-      // this.world.render();
 
       this.player.update();
       this.player.render();
